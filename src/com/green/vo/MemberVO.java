f4195624769179fbc82e38bdbdb552fb80c57528
Mamwalu8 <105907915+Mamwalu8@users.noreply.github.com>
2022-06-28 17:17:06 +0900

    Update MemberVO.java

--- src/com/green/vo/MemberVO.java
+++ src/com/green/vo/MemberVO.java
@@ -1,47 +1,51 @@
 package com.green.vo;
 
 public class MemberVO {
-	private String M_No;
-	private String M_Name;
-	private String P_Code;
-	private String P_School;
-	private String M_Jumin;
-	private String M_City;
+	private String m_No;
+	private String m_Name;
+	private String p_Code;
+	private String p_School;
+	private String m_Jumin;
+	private String m_City;
 	public String getM_No() {
-		return M_No;
+		return m_No;
 	}
 	public void setM_No(String m_No) {
-		M_No = m_No;
+		this.m_No = m_No;
 	}
 	public String getM_Name() {
-		return M_Name;
+		return m_Name;
 	}
 	public void setM_Name(String m_Name) {
-		M_Name = m_Name;
+		this.m_Name = m_Name;
 	}
 	public String getP_Code() {
-		return P_Code;
+		return p_Code;
 	}
 	public void setP_Code(String p_Code) {
-		P_Code = p_Code;
+		this.p_Code = p_Code;
 	}
 	public String getP_School() {
-		return P_School;
+		return p_School;
 	}
 	public void setP_School(String p_School) {
-		P_School = p_School;
+		this.p_School = p_School;
 	}
 	public String getM_Jumin() {
-		return M_Jumin;
+		return m_Jumin;
 	}
 	public void setM_Jumin(String m_Jumin) {
-		M_Jumin = m_Jumin;
+		this.m_Jumin = m_Jumin;
 	}
 	public String getM_City() {
-		return M_City;
+		return m_City;
 	}
 	public void setM_City(String m_City) {
-		M_City = m_City;
+		this.m_City = m_City;
 	}
+	
+	
+	
+
 	
 }
